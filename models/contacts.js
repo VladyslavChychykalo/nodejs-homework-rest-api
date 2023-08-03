58a6245436ef0fcd241b3a21c129c1fa21baca1a
Vladyslav <vladyslav@MacBook-Air-Vladyslav.local>
2023-08-03 14:57:56 +0300

    update schema

--- models/contacts.js
+++ models/contacts.js
@@ -32,10 +32,10 @@ const addSchema = Joi.object({
 });
 
 const updateSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+}).min(1);
 
 const updateFavoriteSchema = Joi.object({
   favorite: Joi.boolean()
